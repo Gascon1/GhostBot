e0a6a5a4e353e73ce8dfa9399ec399e2020755a9
Mikel Gascon <mikelgascon@outlook.com>
2024-11-10 23:32:09 -0500

    add custom revive coins, format files
    Co-authored-by: Denys Melyukhov <MelyukhovD@users.noreply.github.com>

--- commands/dice-roll/revive.js
+++ commands/dice-roll/revive.js
@@ -11,13 +11,68 @@ module.exports = {
    */
   async execute(interaction) {
     const rollResults = rollDice(4, 2);
-    const tableString = createAsciiTable(4, 2, rollResults);
+    const colorOrder = ['blue', 'red', 'yellow', 'green'];
+    let displayedCoins = [
+      meepleEmojis.blue.coinFlipping,
+      meepleEmojis.red.coinFlipping,
+      meepleEmojis.yellow.coinFlipping,
+      meepleEmojis.green.coinFlipping,
+    ];
+    let resultsShown = 0;
 
-    await interaction.reply('```' + '\n' + tableString + '```');
+    await interaction.reply('.');
+    await interaction.deleteReply();
+
+    const message = await interaction.channel.send(`<@${interaction.user.id}>'s fate is being discussed...`);
+
+    await interaction.channel.send(
+      `${meepleEmojis.blue.meeple} ${meepleEmojis.red.meeple} __**Revival Council**__ ${meepleEmojis.yellow.meeple} ${meepleEmojis.green.meeple}`,
+    );
+
+    await interaction.channel.send(displayedCoins.join(' ')).then((msg) => {
+      const rollingCoinsInterval = setInterval(async () => {
+        displayedCoins[resultsShown] =
+          rollResults[resultsShown] === 1 ? oopsCoin : meepleEmojis[colorOrder[resultsShown]].coinStatic;
+
+        await msg.edit(displayedCoins.join(' '));
+        if (resultsShown >= 3) {
+          message.edit(`<@${interaction.user.id}>'s fate has been sealed`);
+          clearInterval(rollingCoinsInterval);
+        }
+        resultsShown += 1;
+      }, 1000);
+    });
 
     const { member } = interaction;
     const deadRole = interaction.guild.roles.cache.find((role) => role.name === 'Dead');
 
-    await updateUserRoleAndNickname(member, rollResults, deadRole);
+    setTimeout(async () => {
+      await updateUserRoleAndNickname(member, rollResults, deadRole);
+    }, 4000);
   },
 };
+
+const meepleEmojis = {
+  blue: {
+    meeple: '<:blueMeeple:1305234250739744798>',
+    coinFlipping: '<a:blueFlip:1305327395900883034>',
+    coinStatic: '<:blue1:1305250818240282714>',
+  },
+  red: {
+    meeple: '<:redMeeple:1305234281727004783>',
+    coinFlipping: '<a:redFlip:1305327396852858942>',
+    coinStatic: '<:red1:1305250820526313553>',
+  },
+  yellow: {
+    meeple: '<:yellowMeeple:1305234312957919283>',
+    coinFlipping: '<a:yellowFlip:1305337174052311051>',
+    coinStatic: '<:yellow1:1305250821482479636>',
+  },
+  green: {
+    meeple: '<:greenMeeple:1305234202551128167>',
+    coinFlipping: '<a:greenFlip:1305323911940018268>',
+    coinStatic: '<:green1:1305250818924220437>',
+  },
+};
+
+const oopsCoin = '<:oopsCoin:1305251655612366930>';
--- commands/dice-roll/roll.js
+++ commands/dice-roll/roll.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const { rollDice } = require('../../lib/dice-roll');
 const { createAsciiTable } = require('../../lib/create-ascii-table');
 const { updateUserRoleAndNickname } = require('../../lib/update-user-role-and-nickname');
-const { determineAsciiArt } = require('../../lib/determine-ascii-art')
+const { determineAsciiArt } = require('../../lib/determine-ascii-art');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -23,13 +23,12 @@ module.exports = {
     const rolls = interaction.options.getInteger('rolls');
     const sides = interaction.options.getInteger('sides');
     const deadRole = interaction.guild.roles.cache.find((role) => role.name === 'Dead');
+
     const rollResults = rollDice(rolls, sides);
     const tableString = createAsciiTable(rolls, sides, rollResults);
-    const asciiMessage = determineAsciiArt(rollResults, member, deadRole)
+    const asciiMessage = determineAsciiArt(rollResults, member, deadRole);
+
     await interaction.reply('```' + '\n' + asciiMessage + '\n' + tableString + '```');
-
-
-
 
     await updateUserRoleAndNickname(member, rollResults, deadRole);
   },
